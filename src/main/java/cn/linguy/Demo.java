339d396b6603caa8a5acc487c5d521dcb64adaaf
molu <1140877376@qq.com>
2019-10-14 14:55:21 +0800

    add message

--- src/main/java/cn/linguy/Demo.java
+++ src/main/java/cn/linguy/Demo.java
@@ -2,9 +2,11 @@ package cn.linguy;
 
 public class Demo {
     public static void main(String[] args) {
+        //haha
         System.out.println("hello github");
     }
     public void init(){
         System.out.println("init");
+        System.out.println("bendangered");
     }
 }
