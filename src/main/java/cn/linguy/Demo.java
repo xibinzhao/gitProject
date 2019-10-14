94787011f3768c2f1ef0fde1ef997cd1b7dae123
molu <1140877376@qq.com>
2019-10-14 14:46:48 +0800

    create method init

--- src/main/java/cn/linguy/Demo.java
+++ src/main/java/cn/linguy/Demo.java
@@ -4,4 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello github");
     }
+    public void init(){
+        System.out.println("init");
+    }
 }
